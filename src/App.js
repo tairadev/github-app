489493ebc55337c33d6c6ae09ed2046a4327ebc5
Guilherme Henrique Taira <guilhermeht1906@gmail.com>
2021-12-03 01:21:33 -0300

    Commit inicial do app

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './scss/style.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import AppContent from './components/app-content';
+import ajax from '@fdaciuk/ajax';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userinfo: null,
+      repos: [],
+      starred: [],
+      isFetching: false
+    }
+  }
+
+  getGitHubApiUrl(username, type) {
+    const internalType = type ? `/${type}` : ""
+    const internalUser = username ? `/${username}` : ""
+    return `https://api.github.com/users${internalUser}${internalType}`
+  }
+
+  handleSearch (e) {
+    const value = e.target.value
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+
+    if(keyCode === ENTER) {
+      this.setState({ isFetching: true })
+      ajax().get(this.getGitHubApiUrl(value))
+      .then((result) => {
+        this.setState({
+          userinfo: {
+            username: result.name,
+            login: result.login,
+            repos: result.public_repos,
+            followers: result.followers,
+            following: result.following,
+            avatar: result.avatar_url
+          },
+          repos: [],
+          starred: []
+        })
+      })
+      .always(() => this.setState({ isFetching: false }))
+    }
+  }
+
+  getRepos(type) {
+    return () => {
+      const username = this.state.userinfo.login
+      const altType = type === "repos" ? "starred" : "repos";
+      this.setState({
+        [altType]: []
+      })
+      ajax().get(this.getGitHubApiUrl(username, type))
+      .then((result) => {
+        this.setState({
+          [type]: result.map((repo) => {
+            return {
+              name: repo.name,
+              link: repo.html_url,
+              language: repo.language
+            }
+          })
+        })
+      })
+    }
+  }
+
+  render() {
+    return (
+      <AppContent 
+        userinfo={this.state.userinfo}
+        repos={this.state.repos}
+        starred={this.state.starred}
+        isFetching={this.state.isFetching}
+        handleSearch={(e) => this.handleSearch(e)}
+        getRepos={this.getRepos('repos')}
+        getStarred={this.getRepos('starred')}
+      />
+    );
+  }
+}
+
+export default App;
